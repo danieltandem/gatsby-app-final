37f58f16c8f8bcdc7ec135d1555c844362116ea3
danieltandem <tandem.danielvargas@gmail.es>
2024-08-27 11:15:35 +0200

    diosss

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,21 @@
+// 404.js
 import React from "react"
 import { Link } from "gatsby"
 
 const NotFoundPage = () => {
-  const isClient = typeof window !== "undefined"
-
   return (
     <div>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      {isClient && (
-        <div>
-          {/* Aquí podrías poner cualquier código que necesite acceso a window o document */}
-          <p>Contenido que solo se muestra en el cliente</p>
-        </div>
-      )}
+      <p>You just hit a route that doesn't exist... the sadness.</p>
       <Link to="/">Go Home</Link>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  }
 }
 
 export default NotFoundPage
